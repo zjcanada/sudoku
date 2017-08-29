5d486efa049c19e28cd98a3f2aa2a667138cfee6
zjcanada <zjcanada@gmail.com>
2017-08-29 09:42:20 -0700

    add some comments; print lines to separate small matrixes.

--- sudoku.c
+++ sudoku.c
@@ -1,8 +1,44 @@
+/* game of SUDOKU
+ * 9 rows x 9 column matrix:
+ * each row consists of 1 ~ 9 digits;
+ * each column same (consists of 1 ~ 9);
+ * 3 x 3 small matrixes starting at (1,1), (1,4), (1,7), (4,1), (4,4), ... contains 1 ~ 9 too.
+ *
+ * to enter 9 x 9 digits without error is quite a work.
+ * so, you can prepare the input data in a text editor, then copy to program console input.
+ * like this:
+
+0 0 0 0 0 0 0 4 0
+8 7 0 1 9 3 0 0 0
+3 0 0 0 5 0 0 0 7
+4 0 0 3 0 0 0 0 8
+1 0 6 0 0 0 5 0 3
+5 0 0 0 0 8 0 0 6
+9 0 0 0 8 0 0 0 4
+0 0 0 4 3 9 0 7 1
+0 4 0 0 0 0 0 0 0
+
+ * Here is the program output:
+ *
+2 5 1 | 8 7 6 | 3 4 9
+8 7 4 | 1 9 3 | 6 5 2
+3 6 9 | 2 5 4 | 1 8 7
+- - - | - - - | - - -
+4 9 2 | 3 6 5 | 7 1 8
+1 8 6 | 7 4 2 | 5 9 3
+5 3 7 | 9 1 8 | 4 2 6
+- - - | - - - | - - -
+9 1 3 | 5 8 7 | 2 6 4
+6 2 5 | 4 3 9 | 8 7 1
+7 4 8 | 6 2 1 | 9 3 5
+ *
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int matrix[10][10]; /* the 9 x 9 matrix */
-int fixed[10][10]; /* initial given numbers */
+int fixed[10][10]; /* initial given numbers, they are fixed and not to change. */
 
 int row_adding(int n)
 {
@@ -16,7 +52,7 @@ int col_adding(int n)
 
 int collision_square(int i, int j)
 {
-    // (i,j) can only be:
+    // (i,j) can only be: (small matrix starting position)
     // (1,1), (1,4), (1,7)
     // (4,1), (4,4), (7,4)
     // (7,1), (7,4), (7,7)
@@ -56,7 +92,7 @@ int collision(int i, int j)
             if (matrix[k][j] == matrix[m][j] && matrix[k][j] != 0)
                 return 1; // collision
 
-    // check the square
+    // check the small matrix
     start_row = ((i-1) % 3)*3 +1;
     start_col = ((j-1) % 3)*3 +1;
     return collision_square(start_row, start_col);
@@ -67,9 +103,20 @@ void print_matrix()
     int i, j;
 
     for (i=1; i<=9; ++i) {
-        for (j=1; j<=9; ++j)
+        for (j=1; j<=9; ++j) {
             printf("%d ", matrix[i][j]);
+            if (j==3 || j==6)
+               printf("| ");
+        }
         printf("\n");
+        if (i==3 || i== 6) {
+            for (j=1; j<=9; ++j) {
+                printf("%c ", '-');
+                if (j==3 || j==6)
+                    printf("%c ", '|');
+            }
+            printf("\n");
+        }
     }
 }
 
